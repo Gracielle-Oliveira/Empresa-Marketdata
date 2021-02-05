565c48af33997cbcc51790151e7befc7cd90de5a
Gracielle Oliveira <gra2gso@gmail.com>
2021-02-05 01:19:55 -0300

    Inicialização da réplica de uma das páginas da empresa Marketdata

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { ListComponent } from './list/list.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArticlesComponent,
+    ListComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
